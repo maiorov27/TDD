983af54ece55c88c7e61e15f24259f0901853e10
Egor Maiorov <maiorov.prog@gmail.com>
2014-09-21 20:09:04 +0300

    created inner class Pair and new HashMap for currensy rates.

--- src/main/java/app/Bank.java
+++ src/main/java/app/Bank.java
@@ -1,6 +1,10 @@
 package app;
 
+import java.util.HashMap;
+
 public class Bank {
+
+    private HashMap<Pair, Integer> rates = new HashMap<Pair, Integer>();
 
     public Money reduce(Expression source, String to) {
 //        if( source instanceof  Money) {return (Money) source.reduce(to);}
@@ -13,5 +17,28 @@ public class Bank {
                 ? 2
                 : 1;
 
+    }
+
+    public void addRate(String from, String to, int value) {
+
+    }
+
+    private class Pair {
+        String from;
+        String to;
+
+        public Pair(String from, String to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        public boolean equals(Object o) {
+            Pair pair = (Pair) o;
+            return pair.to.equals(to) && pair.from.equals(from);
+        }
+
+        public int hashCode() {
+            return 0;
+        }
     }
 }
